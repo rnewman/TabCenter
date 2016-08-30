6d9e0420ef6f857ed934442ed86b1c60fdf2a727
Blake Winton <bwinton@latte.ca>
2016-08-30 11:58:01 -0400

    Don't register clicks on the leftbar if it's not open.

--- verticaltabs.js
+++ verticaltabs.js
@@ -552,6 +552,13 @@ VerticalTabs.prototype = {
     leftbox.addEventListener('mouseenter', enter);
     leftbox.addEventListener('mousemove', enter);
     leftbox.addEventListener('mouseleave', pauseBeforeExit);
+    leftbox.addEventListener('mousedown', () => {
+      // Don't register clicks on the leftbar if it's not open.
+      if (event.mozInputSource === Ci.nsIDOMMouseEvent.MOZ_SOURCE_TOUCH &&
+          leftbox.getAttribute('expanded') !== 'true') {
+        event.stopPropagation();
+      }
+    });
 
     let oldUpdateToolbars = window.FullScreen._updateToolbars;
     window.FullScreen._updateToolbars = (aEnterFS) => {
